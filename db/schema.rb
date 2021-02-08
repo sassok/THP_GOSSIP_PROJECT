406617f3d86bb98d136f4fd48d96a49d066ea7c1
sassok <kevin.sassoo@gmail.com>
2021-02-08 19:44:59 +0100

    More users info

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,10 @@ ActiveRecord::Schema.define(version: 2021_02_08_100002) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
     t.string "name"
+    t.integer "age"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Gossip.destroy_all
 
 require 'faker'
 10.times do |f|
-  users = User.create!(name: Faker::Name.first_name)
+  users = User.create!(name: Faker::Name.first_name, first_name: Faker::Name.last_name, age: Faker::Number.between(from: 17, to: 50), email: Faker::Internet.email)
   gossips = Gossip.create!(title: Faker::Restaurant.name, content: Faker::Restaurant.description, user: User.find(f+1))
   gossips = Gossip.create!(title: Faker::Restaurant.name, content: Faker::Restaurant.description, user: User.find(f+1))
 end
